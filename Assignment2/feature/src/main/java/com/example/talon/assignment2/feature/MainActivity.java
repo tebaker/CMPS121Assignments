ce2e9288965bab93c393115813b03a5e3da1c7a3
Talon Baker <taalonn@gmail.com>
2018-05-07 14:47:52 -0700

    added 'add event' buton and 'go back' buttons to two activitie pages

--- Assignment2/feature/src/main/java/com/example/talon/assignment2/feature/MainActivity.java
+++ Assignment2/feature/src/main/java/com/example/talon/assignment2/feature/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.talon.assignment2.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configAddEventButton();
+    }
+
+    private void configAddEventButton() {
+        Button addEventButton = (Button) findViewById(R.id.addEventBotton);
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddEventActivity.class));
+            }
+        });
     }
 }
